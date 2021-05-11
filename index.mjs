30cc0e9afc309b7ae75a1769633c04580054f9b5
AshWilfern <sguselnikov558@gmail.com>
2021-05-11 16:46:19 +0500

    1-6 Tasks done

--- index.mjs
+++ index.mjs
@@ -7,6 +7,30 @@ const rootDir = process.cwd();
 const port = 3000;
 const app = express();
 
+app.use(express.static("static"));
+
+const coffee_list = [
+  {
+    name: "Americano",
+    image: "/img/americano.jpg",
+    price: 999,
+  },
+  { 
+    name: "Cappuccino",
+    image: "/img/cappuccino.jpg",
+    price: 999 
+  },
+  {
+    name: "Espresso",
+    image: "/img/espresso.jpg",
+    price: 999
+  },
+  { 
+    name: "Latte", 
+    image: "/img/latte.jpg",
+    price: 999
+  },
+];
 // Выбираем в качестве движка шаблонов Handlebars
 app.set("view engine", "hbs");
 // Настраиваем пути и дефолтный view
@@ -21,37 +45,40 @@ app.engine(
 );
 
 app.get("/", (_, res) => {
-  res.sendFile(path.join(rootDir, "/static/html/index.html"));
+  //res.sendFile(path.join(rootDir, "/static/html/index.html"));
+  res.redirect("/menu").end();
 });
 
 app.get("/menu", (_, res) => {
   res.render("menu", {
     layout: "default",
-    items: [
-      {
-        name: "Americano",
-        image: "/static/img/americano.jpg",
-        price: 999,
-      },
-      { name: "Cappuccino", image: "/static/img/cappuccino.jpg", price: 999 },
-    ],
+    items: coffee_list,
   });
 });
 
+let order_list = [];
+
 app.get("/buy/:name", (req, res) => {
-  res.status(501).end();
+  let new_coffee = { ...(coffee_list.find(item => item.name === req.params.name)) };
+  order_list.push(new_coffee);
+  res.redirect("/menu").end();
 });
 
-app.get("/cart", (req, res) => {
-  res.status(501).end();
+app.get("/cart", (_, res) => {
+  res.render("cart", {
+    layout: "default",
+    sum: order_list.reduce((sum, coffee) => sum + coffee.price, 0),
+    items: order_list,
+  });
 });
 
 app.post("/cart", (req, res) => {
-  res.status(501).end();
+  order_list = [];
+  res.redirect("/menu");
 });
 
 app.get("/login", (req, res) => {
-  res.status(501).end();
+  res.sendFile(path.join(rootDir, "/static/html/login.html"));
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
