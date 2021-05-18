1090efe39c26fae8eade1b065215894025937548
sashapetrova28 <sasha_281200@mail.ru>
2021-05-18 16:56:44 +0500

    Петрова Александра задания 1-7

--- index.mjs
+++ index.mjs
@@ -6,52 +6,93 @@ import cookieParser from "cookie-parser";
 const rootDir = process.cwd();
 const port = 3000;
 const app = express();
+const drinks = [
+    {
+        name: "Americano",
+        image: "/static/img/americano.jpg",
+        price: 999,
+    },
+    {
+        name: "Cappuccino",
+        image: "/static/img/cappuccino.jpg",
+        price: 999,
+    },
+    {
+        name: "Espressoo",
+        image: "/static/img/espresso.jpg",
+        price: 100,
+    },
+]
+
+app.use("/static", express.static("static"));
 
 // Выбираем в качестве движка шаблонов Handlebars
 app.set("view engine", "hbs");
 // Настраиваем пути и дефолтный view
 app.engine(
-  "hbs",
-  hbs({
-    extname: "hbs",
-    defaultView: "default",
-    layoutsDir: path.join(rootDir, "/views/layouts/"),
-    partialsDir: path.join(rootDir, "/views/partials/"),
-  })
+    "hbs",
+    hbs({
+        extname: "hbs",
+        defaultView: "default",
+        layoutsDir: path.join(rootDir, "/views/layouts/"),
+        partialsDir: path.join(rootDir, "/views/partials/"),
+    })
 );
 
 app.get("/", (_, res) => {
-  res.sendFile(path.join(rootDir, "/static/html/index.html"));
+    res.sendFile(path.join(rootDir, "/static/html/index.html"));
+    res.redirect("/menu");
 });
 
 app.get("/menu", (_, res) => {
-  res.render("menu", {
-    layout: "default",
-    items: [
-      {
-        name: "Americano",
-        image: "/static/img/americano.jpg",
-        price: 999,
-      },
-      { name: "Cappuccino", image: "/static/img/cappuccino.jpg", price: 999 },
-    ],
-  });
+    res.render("menu", {
+        layout: "default",
+        items: drinks,
+    });
 });
 
 app.get("/buy/:name", (req, res) => {
-  res.status(501).end();
+    res.redirect("/menu");
+    cart.push(drinks.find(() => req.params.name === drinks.name));
+
 });
 
 app.get("/cart", (req, res) => {
-  res.status(501).end();
+    res.render("cart", {
+        layout: "default",
+        items: cart,
+        totalPrice: cart.reduce((count, next) => (count + next.price), 0),
+    });
 });
 
 app.post("/cart", (req, res) => {
-  res.status(501).end();
+    let cart = [];
+    res.render("cart", {
+      layout: "default",
+      items: cart,
+      totalPrice: cart.reduce((count, next) => (count + next.price), 0),
+
+    })
 });
 
 app.get("/login", (req, res) => {
-  res.status(501).end();
+  let username;
+  if(req.query.username) {
+      username = req.query.username;
+      res.cookie("username", username);
+  }
+  if(req.cookies.username) {
+    username = req.cookies.username;
+    res.cookie("username", username);
+  }
+  else if(req.cookies && req.cookies.username) {
+    username = req.cookies.username;
+  }
+  res.render("login", {
+      layout: "default",
+      username: req.cookies.username || username || "Аноним",
+  })
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
+
