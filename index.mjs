af025cd58714e60934de8e46bfa0b7bd58598e46
FreeTrally <39124135+FreeTrally@users.noreply.github.com>
2021-05-30 22:01:40 +0500

    Done

--- index.mjs
+++ index.mjs
@@ -6,7 +6,42 @@ import cookieParser from "cookie-parser";
 const rootDir = process.cwd();
 const port = 3000;
 const app = express();
+const coffee = [
+	{
+		name: "americano",
+		image: "/static/img/americano.jpg",
+		price: 60,
+	},
+	{
+		name: "cappuccino",
+		image: "/static/img/cappuccino.jpg",
+		price: 90,
+	},
+	{
+		name: "espresso",
+		image: "/static/img/espresso.jpg",
+		price: 75,
+	},
+	{
+		name: "flat-white",
+		image: "/static/img/flat-white.jpg",
+		price: 80,
+	},
+  {
+		name: "latte-macchiato",
+		image: "/static/img/latte-macchiato.jpg",
+		price: 100,
+	},
+	{
+		name: "latte",
+		image: "/static/img/latte.jpg",
+		price: 85,
+	}	
+];
+const users = {};
 
+app.use("/static", express.static("static"));
+app.use(cookieParser());
 // Выбираем в качестве движка шаблонов Handlebars
 app.set("view engine", "hbs");
 // Настраиваем пути и дефолтный view
@@ -22,36 +57,102 @@ app.engine(
 
 app.get("/", (_, res) => {
   res.sendFile(path.join(rootDir, "/static/html/index.html"));
+  res.redirect("/menu");
 });
 
 app.get("/menu", (_, res) => {
   res.render("menu", {
     layout: "default",
-    items: [
-      {
-        name: "Americano",
-        image: "/static/img/americano.jpg",
-        price: 999,
-      },
-      { name: "Cappuccino", image: "/static/img/cappuccino.jpg", price: 999 },
-    ],
+    items: coffee,
+    title: "Menu"
   });
 });
 
 app.get("/buy/:name", (req, res) => {
-  res.status(501).end();
+  const user = getCurrentUser(req);
+  if (!user) {
+		res.redirect("/login");
+		return;
+	}
+	user.cart.push(coffee.find(x => x.name === req.params.name));
+	res.redirect("/menu");
 });
+
+function getCurrentUser(res) {
+	const name = res.cookies.name;
+	if (name in users) {
+		return users[name];
+	}
+	return null;
+}
 
 app.get("/cart", (req, res) => {
-  res.status(501).end();
+  const user = getCurrentUser(req);
+  if (!user) {
+		res.redirect("/login");
+		return;
+	}
+  res.render("cart", {
+		layout: "default",
+		total: getTotal(user.cart || []),
+		items: user.cart || [],
+		title: "Cart",
+	});
 });
 
+function getTotal(user_cart) {
+	let sum = 0;
+	for (const product of user_cart) {
+		sum += product.price;
+	}
+	return sum;
+}
+
 app.post("/cart", (req, res) => {
-  res.status(501).end();
+  const user = getCurrentUser(req);
+  if (!user) {
+		res.redirect("/login");
+		return;
+	}
+	user.history.push({
+		cart: user.cart,
+		id: user.id++
+	});
+	user.cart = [];
+	res.redirect("/cart");
 });
 
 app.get("/login", (req, res) => {
-  res.status(501).end();
+  if (req.query.username) {
+		if (!(req.query.username in users)) {
+			const user = {
+				name: req.query.username,
+				cart: [],
+        history: [],
+				id: 1
+			};
+			users[user.name] = user;
+		}
+		res.cookie("name", req.query.username);
+	}
+	res.render("login", {
+		layout: "default",
+		username: req.cookies.name || "username",
+    title: "Login",
+	});
+});
+
+app.get("/history", function (req, res) {
+	const user = getCurrentUser(req);
+	if (!user) {
+		res.redirect("/login");
+		return;
+	}
+	res.render("history", {
+		layout: "default",
+		title: "History",
+		items: user.history,
+	});
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
--- static/js/theme.mjs
+++ static/js/theme.mjs
@@ -3,6 +3,15 @@ const root = document.querySelector(":root");
 const defaultState = root.classList.contains("dark");
 
 themeSwitch.checked = defaultState;
+if (document.cookie.includes("theme=dark")) {
+  root.classList.toggle("dark");
+  themeSwitch.checked = true;
+}
 themeSwitch.addEventListener("click", () => {
   root.classList.toggle("dark");
+  if (root.classList.contains("dark")) {
+    document.cookie = "theme=dark"
+  } else {
+    document.cookie = "theme=light";
+  }
 });
