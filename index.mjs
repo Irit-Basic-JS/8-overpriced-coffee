044b3d5e088fe57d9b5c77000bd228b8ead301c3
bozhkovkost <71607999+bozhkovkost@users.noreply.github.com>
2021-06-08 01:40:42 +0500

    index.mjs

--- index.mjs
+++ index.mjs
@@ -2,11 +2,11 @@ import express from "express";
 import * as path from "path";
 import hbs from "express-handlebars";
 import cookieParser from "cookie-parser";
-
 const rootDir = process.cwd();
 const port = 3000;
 const app = express();
 
+app.use('/static', express.static('static'));
 // Выбираем в качестве движка шаблонов Handlebars
 app.set("view engine", "hbs");
 // Настраиваем пути и дефолтный view
@@ -21,7 +21,7 @@ app.engine(
 );
 
 app.get("/", (_, res) => {
-  res.sendFile(path.join(rootDir, "/static/html/index.html"));
+  res.redirect('/menu'); 
 });
 
 app.get("/menu", (_, res) => {
@@ -33,25 +33,45 @@ app.get("/menu", (_, res) => {
         image: "/static/img/americano.jpg",
         price: 999,
       },
-      { name: "Cappuccino", image: "/static/img/cappuccino.jpg", price: 999 },
+      { name: "Cappuccino", image: "/static/img/cappuccino.jpg", price: 999 }, 
+      {
+        name: "Latte", 
+        image: "/static/img/latte.jpg",
+        price: 999,
+      },
+      {
+        name: "Espresso", 
+        image: "/static/img/espresso.jpg",
+        price: 999,
+      },
+      {
+        name: "Flat-white", 
+        image: "/static/img/flat-white.jpg",
+        price: 999,
+      },
+      {
+        name: "Latte-macchiato", 
+        image: "/static/img/latte-macchiato.jpg",
+        price: 999,
+      },
     ],
   });
 });
-
-app.get("/buy/:name", (req, res) => {
-  res.status(501).end();
+	@@ -43,7 +64,11 @@ app.get("/buy/:name", (req, res) => {
 });
 
 app.get("/cart", (req, res) => {
-  res.status(501).end();
+  res.render("cart", {
+    layout: "default",
+    items: [
+    ],
+  });
 });
 
 app.post("/cart", (req, res) => {
   res.status(501).end();
 });
-
 app.get("/login", (req, res) => {
   res.status(501).end();
 });
-
 app.listen(port, () => console.log(`App listening on port ${port}`));
