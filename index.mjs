8818cbd2aa466ee26b039f78311866bc5c34c895
dark7py <matveym20022002@gmail.com>
2021-11-26 19:39:28 +0500

    1-5

--- index.mjs
+++ index.mjs
@@ -6,6 +6,40 @@ import cookieParser from "cookie-parser";
 const rootDir = process.cwd();
 const port = 3000;
 const app = express();
+let sum = 0;
+
+const items = {
+  "Americano": {
+    name: "Americano",
+    image: '/static/img/americano.jpg',
+    price: 999,
+  },
+  "Cappuccino": { 
+    name: "Cappuccino", 
+    image: "/static/img/cappuccino.jpg", 
+    price: 999 
+  },
+  "Espresso": {
+    name: "Espresso",
+    image: '/static/img/espresso.jpg',
+    price: 111,
+  },
+  "Flat white": {
+    name: "Flat white",
+    image: '/static/img/flat-white.jpg',
+    price: 222,
+  },
+  "Latte": {
+    name: "Latte",
+    image: '/static/img/latte.jpg',
+    price: 333,
+  },
+  "Latte Macchiato": {
+    name: "Latte Macchiato",
+    image: '/static/img/latte-macchiato.jpg',
+    price: 433,
+  },
+};
 
 // Выбираем в качестве движка шаблонов Handlebars
 app.set("view engine", "hbs");
@@ -20,30 +54,49 @@ app.engine(
   })
 );
 
+// 1
+// app.use(express.static('static'))
+
+// 2
+app.use('/static', express.static('static'));
+// console.log(rootDir);
+// 
 app.get("/", (_, res) => {
-  res.sendFile(path.join(rootDir, "/static/html/index.html"));
+  res.redirect('/menu')
+ // res.sendFile(path.join(rootDir, "/static/html/index.html"));
 });
 
 app.get("/menu", (_, res) => {
   res.render("menu", {
     layout: "default",
-    items: [
-      {
-        name: "Americano",
-        image: "/static/img/americano.jpg",
-        price: 999,
-      },
-      { name: "Cappuccino", image: "/static/img/cappuccino.jpg", price: 999 },
-    ],
+    items: Object.values(items),
+  });
+});
+// cart2
+app.get("/buy/:name", (req, res) => {
+  let item = items[req.params.name];
+  cartContents.push(item);
+  sum += item.price;
+  res.redirect('/menu');
+});
+
+const cartContents = [];
+
+app.get("/cart", (req, res) => {
+  res.render("cart", {
+    layout: "default",
+    cartContents: cartContents,
+    sum: sum,
   });
 });
 
-app.get("/buy/:name", (req, res) => {
-  res.status(501).end();
-});
 
-app.get("/cart", (req, res) => {
-  res.status(501).end();
+app.get("/cart2", (req,res) => {
+  res.render("cart2", {
+    layout: "default",
+    cartContents: [],
+    sum: sum,
+  });
 });
 
 app.post("/cart", (req, res) => {
