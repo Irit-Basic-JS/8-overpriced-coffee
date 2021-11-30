85d8a28ea8a8218d46817ed9880103a58260d33c
angst-storm <Sergey.Kiprin@urfu.me>
2021-12-01 02:12:35 +0500

    Выполнены задания 0-11

--- static/js/theme.mjs
+++ static/js/theme.mjs
@@ -1,8 +1,28 @@
 const themeSwitch = document.querySelector(".theme__switch input");
 const root = document.querySelector(":root");
-const defaultState = root.classList.contains("dark");
+
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function setCookie(name, value, options = {}) {
+  document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+}
+
+const defaultState = getCookie('theme') === 'dark';
 
 themeSwitch.checked = defaultState;
+if (defaultState)
+  root.classList.add("dark");
+
 themeSwitch.addEventListener("click", () => {
   root.classList.toggle("dark");
+  const isDark = root.classList.contains('dark');
+  if (isDark)
+    setCookie('theme', 'dark');
+  else
+    setCookie('theme', 'light');
 });
