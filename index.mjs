19d4286aed4a44f992906e495d3ee3be863ae960
Teelirium <74907594+Teelirium@users.noreply.github.com>
2021-12-01 18:42:42 +0500

    Задания 1-11

--- index.mjs
+++ index.mjs
@@ -11,47 +11,137 @@ const app = express();
 app.set("view engine", "hbs");
 // Настраиваем пути и дефолтный view
 app.engine(
-  "hbs",
-  hbs({
-    extname: "hbs",
-    defaultView: "default",
-    layoutsDir: path.join(rootDir, "/views/layouts/"),
-    partialsDir: path.join(rootDir, "/views/partials/"),
-  })
+    "hbs",
+    hbs({
+        extname: "hbs",
+        defaultView: "default",
+        layoutsDir: path.join(rootDir, "/views/layouts/"),
+        partialsDir: path.join(rootDir, "/views/partials/"),
+    })
 );
 
+app.use('/static', express.static('static'));
+app.use(cookieParser());
+
 app.get("/", (_, res) => {
-  res.sendFile(path.join(rootDir, "/static/html/index.html"));
+    res.redirect("/menu");
 });
 
-app.get("/menu", (_, res) => {
-  res.render("menu", {
-    layout: "default",
-    items: [
-      {
-        name: "Americano",
-        image: "/static/img/americano.jpg",
-        price: 999,
-      },
-      { name: "Cappuccino", image: "/static/img/cappuccino.jpg", price: 999 },
-    ],
-  });
+function Coffee(name, image, price) {
+    return {
+        name, image, price
+    };
+}
+
+const coffeeTypes = [
+    Coffee("Americano", "/static/img/americano.jpg", 999),
+    Coffee("Cappuccino", "/static/img/cappuccino.jpg", 999),
+    Coffee("Espresso", "/static/img/espresso.jpg", 998),
+    Coffee("Flat-white", "/static/img/flat-white.jpg", 999),
+    Coffee("Latte", "/static/img/latte.jpg", 999),
+    Coffee("Latte-macchiato", "/static/img/latte-macchiato.jpg", 1000),
+];
+
+let cart = {};
+
+let history = {};
+
+function HistoryEntry(items, date) {
+    return {
+        items, date
+    };
+}
+
+function getUsername(req) {
+    return req.cookies.username || "Аноним";
+}
+
+function getTheme(req) {
+    return req.cookies.theme || "light";
+}
+
+app.get("/menu", (req, res) => {
+    res.render("menu", {
+        layout: "default",
+        title: "Меню",
+        theme: getTheme(req),
+        items: coffeeTypes,
+    });
 });
 
 app.get("/buy/:name", (req, res) => {
-  res.status(501).end();
+    let name = getUsername(req);
+
+    if (cart[name] === undefined)
+        cart[name] = [];
+
+    cart[name].push(coffeeTypes.filter(c => c.name == req.params["name"])[0])
+
+    res.redirect("/menu");
 });
 
 app.get("/cart", (req, res) => {
-  res.status(501).end();
+    let name = getUsername(req);
+
+    if (cart[name] === undefined)
+        cart[name] = [];
+
+    res.render("cart", {
+        layout: "default",
+        title: "Корзина",
+        theme: getTheme(req),
+        cart: cart[name],
+        totalCost: cart[name].map(c => c.price).reduce((a, b) => a + b, 0),
+    });
 });
 
 app.post("/cart", (req, res) => {
-  res.status(501).end();
+    let name = getUsername(req);
+
+    if (history[name] === undefined)
+        history[name] = [];
+
+    let date = new Date(Date.now()).toLocaleString("ru-RU");
+    history[name].push(new HistoryEntry(cart[name], date));
+
+    cart[name] = [];
+
+    res.render("cart", {
+        layout: "default",
+        title: "Корзина",
+        theme: getTheme(req),
+        cart: cart[name],
+        totalCost: cart[name].map(c => c.price).reduce((a, b) => a + b, 0),
+    });
 });
 
 app.get("/login", (req, res) => {
-  res.status(501).end();
+    let name = req.query.username || getUsername(req);
+
+    res.cookie("username", name);
+
+	res.render("login", {
+        layout: "default",
+        title: "Личный кабинет",
+        theme: getTheme(req),
+        name: name,
+    });
+});
+
+app.get("/history", (req, res) => {
+    let name = getUsername(req);
+
+    if (history[name] === undefined)
+        history[name] = [];
+
+    history[name].sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
+
+    res.render("history", {
+        layout: "default",
+        title: "История заказов",
+        theme: getTheme(req),
+        history: history[name],
+    });
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
