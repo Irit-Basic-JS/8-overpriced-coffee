0c96710d69218b41c54409b1a225a3575b7f1b2c
debebantur <stepan.filippov@yandex.ru>
2021-12-02 21:55:20 +0500

    1-10

--- static/js/theme.mjs
+++ static/js/theme.mjs
@@ -1,8 +1,28 @@
 const themeSwitch = document.querySelector(".theme__switch input");
 const root = document.querySelector(":root");
-const defaultState = root.classList.contains("dark");
+const defaultState = GetTheme();
+
+if(defaultState)
+  root.classList.add("dark");
+else
+  root.classList.remove("dark");
 
 themeSwitch.checked = defaultState;
 themeSwitch.addEventListener("click", () => {
   root.classList.toggle("dark");
+
+  if (root.classList.contains("dark"))
+  document.cookie = "theme=1";
+  else
+  document.cookie = "theme=0";
 });
+
+function GetTheme(){
+  let parts = document.cookie.split('; ');
+  for(let part of parts){
+    if( part.indexOf('theme')!= -1){
+      return +part.split('=')[1];
+    }
+  }
+  return 0;
+}
