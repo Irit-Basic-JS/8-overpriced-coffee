cd6823c89e27aaab1da508a0540b00953b4e8023
coca-in-a-cola <coca-in-a-cola@protonmail.com>
2021-12-03 00:05:30 +0500

    all tasks done!

--- index.mjs
+++ index.mjs
@@ -6,6 +6,41 @@ import cookieParser from "cookie-parser";
 const rootDir = process.cwd();
 const port = 3000;
 const app = express();
+let currentCart = [];
+let sum = 0;
+
+
+let userDataDefault = {
+  theme: 'light',
+  cart: [],
+  sum: 0,
+  name: "Аноним",
+  history: [],
+}
+
+const setUserData = (req, res, data) => {
+  let dataObj = Object.assign(getUserData(req), data);
+  //console.log(dataObj);
+  res.cookie('userData', dataObj)
+}
+
+const getUserData = (req) => {
+  if (req.cookies && 'userData' in req.cookies) {
+    //console.log(req.cookies.userData);
+    return Object.assign({... userDataDefault}, req.cookies.userData);
+  }
+
+  return {... userDataDefault};
+}
+
+const items = {
+  "Americano": { name: "Americano", image: '/static/img/americano.jpg', price: 999 },
+  "Cappuccino": { name: "Cappuccino", image: "/static/img/cappuccino.jpg", price: 899 },
+  "Espresso": { name: "Espresso", image: "/static/img/espresso.jpg", price: 299 },
+  "Flat white": { name: "Flat white", image: "/static/img/flat-white.jpg", price: 599 },
+  "Latte": { name: "Latte", image: "/static/img/latte.jpg", price: 399 },
+  "Latte Macchiato": { name: "Latte Macchiato", image: "/static/img/latte-macchiato.jpg", price: 699 },
+};
 
 // Выбираем в качестве движка шаблонов Handlebars
 app.set("view engine", "hbs");
@@ -20,38 +55,100 @@ app.engine(
   })
 );
 
-app.get("/", (_, res) => {
-  res.sendFile(path.join(rootDir, "/static/html/index.html"));
+app.use('/static', express.static('static'));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.redirect('/menu');
 });
 
-app.get("/menu", (_, res) => {
+app.get("/menu", (req, res) => {
+  
+  let userData = getUserData(req);
   res.render("menu", {
+    title: `Overpriced Coffe`,
+    theme: userData.theme,
     layout: "default",
-    items: [
-      {
-        name: "Americano",
-        image: "/static/img/americano.jpg",
-        price: 999,
-      },
-      { name: "Cappuccino", image: "/static/img/cappuccino.jpg", price: 999 },
-    ],
+    items: Object.values(items),
   });
 });
 
 app.get("/buy/:name", (req, res) => {
-  res.status(501).end();
+  let userData = getUserData(req);
+
+  console.log(userData.sum, typeof userData.sum);
+
+  let item = items[req.params.name];
+  userData.cart.push(item);
+
+  userData.sum = +userData.sum;
+  userData.sum += item.price;
+
+  console.log(userData);
+
+  setUserData(req, res, userData)
+  res.redirect('/menu');
 });
 
 app.get("/cart", (req, res) => {
-  res.status(501).end();
+  let userData = getUserData(req);
+  res.render("cart", {
+    title: `Корзина (${userData.cart.length})`,
+    theme: userData.theme,
+    layout: "default",
+    cart: userData.cart,
+    sum: userData.sum,
+  });
 });
 
 app.post("/cart", (req, res) => {
-  res.status(501).end();
+  let userData = getUserData(req);
+  userData.history.push({sum: userData.sum, cart: userData.cart})
+
+  userData.cart = [];
+  userData.sum = 0;
+
+  setUserData(req, res, userData)
+  res.redirect("/menu");
+});
+
+app.get("/history", (req, res) => {
+  let userData = getUserData(req);
+
+  res.render("history", {
+    title: `История (${userData.history.length})`,
+    theme: userData.theme,
+    layout: "default",
+    history: userData.history,
+  });
 });
 
 app.get("/login", (req, res) => {
-  res.status(501).end();
+  if ('username' in req.query) {
+    let name = req.query.username;
+    setUserData(req, res, {name: name})
+    res.redirect('/login');
+  }
+
+  else {
+    let userData = getUserData(req);
+    res.render("login", {
+      title: `Привет, ${userData.name}`,
+      theme: userData.theme,
+      layout: "default",
+      name: getUserData(req).name,
+    });  
+  }
+});
+
+app.post('/api/switchTheme/:theme', (req, res) => {
+  //
+  let theme = req.params.theme;
+
+  console.log(theme);
+  setUserData(req, res, {theme: theme})
+
+  res.sendStatus(200);
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
--- static/js/theme.mjs
+++ static/js/theme.mjs
@@ -1,8 +1,26 @@
 const themeSwitch = document.querySelector(".theme__switch input");
 const root = document.querySelector(":root");
-const defaultState = root.classList.contains("dark");
 
-themeSwitch.checked = defaultState;
+let currentState = +root.classList.contains("dark");
+
+const states = [
+  'light',
+  'dark',
+]
+
+themeSwitch.checked = currentState;
 themeSwitch.addEventListener("click", () => {
-  root.classList.toggle("dark");
+  
+  currentState++;
+  currentState %= states.length;
+
+  let data = {theme: states[currentState]};
+  console.log(JSON.stringify(data));
+
+  fetch(`/api/switchTheme/${states[currentState]}`, {
+    method: "POST",
+    headers: {'Content-Type': 'application/json'},
+  }).then(res => {
+    root.classList.toggle("dark");
+  })
 });
