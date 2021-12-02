577dc09deb61a803b6577e75580bafbe30511b2b
Alexey <cmango67@gmail.com>
2021-12-02 22:32:00 +0500

    1-6 task done

--- index.mjs
+++ index.mjs
@@ -6,6 +6,18 @@ import cookieParser from "cookie-parser";
 const rootDir = process.cwd();
 const port = 3000;
 const app = express();
+let currentCart = [];
+let sum = 0;
+
+
+const items = {
+  "Americano": { name: "Americano", image: '/static/img/americano.jpg', price: 999 },
+  "Cappuccino": { name: "Cappuccino", image: "/static/img/cappuccino.jpg", price: 899 },
+  "Espresso": { name: "Espresso", image: "/static/img/espresso.jpg", price: 299 },
+  "Flat white": { name: "Flat white", image: "/static/img/flat-white.jpg", price: 599 },
+  "Latte": { name: "Latte", image: "/static/img/latte.jpg", price: 399 },
+  "Latte Macchiato": { name: "Latte Macchiato", image: "/static/img/latte-macchiato.jpg", price: 699 },
+};
 
 // Выбираем в качестве движка шаблонов Handlebars
 app.set("view engine", "hbs");
@@ -20,34 +32,39 @@ app.engine(
   })
 );
 
+app.use('/static', express.static('static'));
+
+
 app.get("/", (_, res) => {
-  res.sendFile(path.join(rootDir, "/static/html/index.html"));
+  res.redirect('/menu');
 });
 
 app.get("/menu", (_, res) => {
   res.render("menu", {
     layout: "default",
-    items: [
-      {
-        name: "Americano",
-        image: "/static/img/americano.jpg",
-        price: 999,
-      },
-      { name: "Cappuccino", image: "/static/img/cappuccino.jpg", price: 999 },
-    ],
+    items: Object.values(items),
   });
 });
 
 app.get("/buy/:name", (req, res) => {
-  res.status(501).end();
+  let item = items[req.params.name];
+  currentCart.push(item);
+  sum += item.price;
+  res.redirect('/menu');
 });
 
 app.get("/cart", (req, res) => {
-  res.status(501).end();
+  res.render("cart", {
+    layout: "default",
+    cart: currentCart,
+    sum: sum,
+  });
 });
 
 app.post("/cart", (req, res) => {
-  res.status(501).end();
+  currentCart = [];
+  sum = 0;
+  res.redirect("/menu");
 });
 
 app.get("/login", (req, res) => {
