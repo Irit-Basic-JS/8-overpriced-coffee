bebf0c007f2f7893ac98fa4507a2d69e4bfd6f9f
Pavuuk <88192739+Pavuuk@users.noreply.github.com>
2021-12-03 16:44:47 +0500

    Новожилова Екатерина

--- static/js/theme.mjs
+++ static/js/theme.mjs
@@ -3,6 +3,8 @@ const root = document.querySelector(":root");
 const defaultState = root.classList.contains("dark");
 
 themeSwitch.checked = defaultState;
+themeSwitch.checked && root.classList.toggle("dark");
 themeSwitch.addEventListener("click", () => {
   root.classList.toggle("dark");
+  document.cookie = `dark_theme=${themeSwitch.checked}`;
 });
