37510bbf0af8bd0d137a40c53582deffedaa86d8
xirce <dgorohov78@gmail.com>
2021-12-03 23:45:02 +0500

    all tasks done.

--- static/js/theme.mjs
+++ static/js/theme.mjs
@@ -1,8 +1,11 @@
 const themeSwitch = document.querySelector(".theme__switch input");
 const root = document.querySelector(":root");
-const defaultState = root.classList.contains("dark");
+const extraTheme = 'dark';
+const defaultState = root.classList.contains(extraTheme);
 
 themeSwitch.checked = defaultState;
+localStorage.getItem('theme') === extraTheme && root.classList.toggle(extraTheme);
 themeSwitch.addEventListener("click", () => {
-  root.classList.toggle("dark");
+    root.classList.toggle(extraTheme);
+    localStorage.setItem('theme', root.classList.contains(extraTheme) ? extraTheme : 'default');
 });
