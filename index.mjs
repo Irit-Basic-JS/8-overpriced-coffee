d32aacbae0ec6529e542afc9f70f1bed425d7e39
RenatZubakin <zubakinr03@mail.ru>
2021-12-03 05:34:05 +0500

    final

--- index.mjs
+++ index.mjs
@@ -7,9 +7,25 @@ const rootDir = process.cwd();
 const port = 3000;
 const app = express();
 
+let CustomersOrders = {};
+let CustomersHistory = {};
+let sum = 0;
+const items = {
+  "Americano": { name: "Americano", image: '/static/img/americano.jpg', price: 999 },
+  "Cappuccino": { name: "Cappuccino", image: "/static/img/cappuccino.jpg", price: 899 },
+  "Espresso": { name: "Espresso", image: "/static/img/espresso.jpg", price: 299 },
+  "Flat white": { name: "Flat white", image: "/static/img/flat-white.jpg", price: 599 },
+  "Latte": { name: "Latte", image: "/static/img/latte.jpg", price: 399 },
+  "Latte Macchiato": { name: "Latte Macchiato", image: "/static/img/latte-macchiato.jpg", price: 699 },
+};
+
 // Выбираем в качестве движка шаблонов Handlebars
 app.set("view engine", "hbs");
 // Настраиваем пути и дефолтный view
+
+app.use('/static',express.static('static'));
+app.use(cookieParser());
+
 app.engine(
   "hbs",
   hbs({
@@ -21,37 +37,74 @@ app.engine(
 );
 
 app.get("/", (_, res) => {
-  res.sendFile(path.join(rootDir, "/static/html/index.html"));
+  res.redirect('/menu');
 });
 
 app.get("/menu", (_, res) => {
   res.render("menu", {
     layout: "default",
-    items: [
-      {
-        name: "Americano",
-        image: "/static/img/americano.jpg",
-        price: 999,
-      },
-      { name: "Cappuccino", image: "/static/img/cappuccino.jpg", price: 999 },
-    ],
+    title: 'OverpricedCoffe',
+    items: Object.values(items) 
   });
 });
 
 app.get("/buy/:name", (req, res) => {
-  res.status(501).end();
+  let user = req.cookies.username || "Аноним";
+    if (!(user in CustomersOrders)) {
+      CustomersOrders[user] = [];
+    }
+    CustomersOrders[user].push(items[req.params.name])
+    res.redirect('/menu');
 });
 
+
 app.get("/cart", (req, res) => {
-  res.status(501).end();
+  let user = req.cookies.username || "Аноним";
+  let order=[];
+  let price=0;
+  if ((user in CustomersOrders)){
+    order = CustomersOrders[user];
+    price = CustomersOrders[user].reduce((s, current) => s + current.price, 0)
+  }
+  
+  res.render("cart", {
+    layout: "default",
+    title: "Корзина",
+    cart: order,
+    sum: price
+  });
 });
 
 app.post("/cart", (req, res) => {
-  res.status(501).end();
+  let user = req.cookies.username || "Аноним";
+  if (!(user in CustomersHistory)) {
+    CustomersHistory[user] = [];
+  }
+   CustomersOrders[user].forEach(element => {
+     CustomersHistory[user].push(element);
+   });
+  CustomersOrders[user] = [];
+  res.redirect("/menu");
 });
 
 app.get("/login", (req, res) => {
-  res.status(501).end();
+  let username = req.query.username || req.cookies.username || "Аноним";
+  res.cookie("username", username);
+  res.render("login", {
+      layout: "default",
+      title: "Авторизация пользователя",
+      username: username
+  });
+});
+
+app.get("/history",(req,res)=>{
+  let user = req.query.username || req.cookies.username || "Аноним";
+  console.log( CustomersHistory[user]);
+  res.render("history", {
+    layout: "default",
+    items: CustomersHistory[user],
+    title: "История заказов",
+  });
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
--- static/js/theme.mjs
+++ static/js/theme.mjs
@@ -1,8 +1,34 @@
 const themeSwitch = document.querySelector(".theme__switch input");
 const root = document.querySelector(":root");
 const defaultState = root.classList.contains("dark");
-
 themeSwitch.checked = defaultState;
+
+makeTheme();
+
 themeSwitch.addEventListener("click", () => {
-  root.classList.toggle("dark");
+  if (localStorage.getItem('themeStyle')=='dark'){
+    localStorage.themeStyle='light';
+    makeTheme();
+
+  }
+  else if (localStorage.getItem('themeStyle')=='light'){
+    localStorage.themeStyle='dark';
+    makeTheme();
+  }
+    console.log(localStorage.getItem('themeStyle'))
 });
+
+function makeTheme(){
+  if(localStorage.getItem('themeStyle')===null){
+    localStorage.setItem('themeStyle','light')
+  }
+  else if (localStorage.getItem('themeStyle')=='dark'){
+    root.classList.add('dark');
+  }
+  else if (localStorage.getItem('themeStyle')=='light'){
+    root.classList.remove('dark');
+  }
+};
+
+
+
